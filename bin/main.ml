32b22c846d8f79e6baa457fed0b39fe396abed55
Tazmilur Saad <ssaad@colgate.edu>
2023-12-02 02:05:38 -0500

    Working implementation

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,46 @@
 open Async
 open Core
+open Incremental
 
 let stdin_reader = force Reader.stdin
 let stdout_writer = force Writer.stdout
 let echo s = Writer.write stdout_writer s
 
+module M = Make ()
+
+let input_char = Var.create M.State.t (Reader.Read_result.return '\x00')
+
+let current_char = Var.watch input_char
+
+let view = map current_char ~f:(function
+    | `Ok flush when Char.equal flush '\x04' -> "\x1b[1DWe are done"
+    | `Ok res -> Format.sprintf "\x1b[1D%c" res
+    | `Eof -> "\x1b[1DWe are done"
+  )
+
+let count = Var.create M.State.t 0
+
 let event_loop () =
-  ignore (Deferred.create (fun finished ->
-    let rec loop () =
-      upon (Reader.read_char stdin_reader) (function
-        | `Ok res -> echo (Format.sprintf "Got character %c\n" res); loop ();
-        | `Eof -> Ivar.fill finished (); echo "We are done!\n")
-    in loop ()
-  ))
+  echo "Got the character:  \x1b[?25l";
+  ignore
+    (Deferred.create (fun finished ->
+         let rec loop () =
+           upon (Reader.read_char stdin_reader) (function
+             | `Ok flush when Char.equal flush '\x04' ->
+                 Ivar.fill finished ();
+                 stabilize M.State.t;
+                 echo
+                   (Format.sprintf "\x1b[1DWe are done! Count was %d"
+                      (Var.value count))
+             | `Ok res ->
+                 echo (Format.sprintf "\x1b[1D%c" res);
+                 Var.set count (Var.value count + 1);
+                 loop ()
+             | `Eof ->
+                 Ivar.fill finished ();
+                 echo "We are done!\n")
+         in
+         loop ()))
 
 let raw_mode () =
   let open Core_unix in
