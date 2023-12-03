c0cadc5bc4f40041d7291833baed056004a72d07
Tazmilur Saad <ssaad@colgate.edu>
2023-12-03 00:44:19 -0500

    Simplify node type

--- lib/dom.ml
+++ lib/dom.ml
@@ -1,25 +1,23 @@
 module Node = struct
   module M = Map.Make (String)
 
-  type t = { key : string; value : string; children : t M.t }
+  type node =
+  { key : string
+  ; value : element
+  ; children : node list
+  }
+  and element =
+    | Int of int
+    | String of string
 
-  let create k v c = { key = k; value = v; children = c }
-  let empty = M.empty
-
-  let add parent node =
-    match parent with
-    | { key = k; value = v; children = c } ->
-        { key = k; value = v; children = M.add node.key node c }
+  let create key value children = { key = key; value = value; children = children }
+  let add child { key; value; children} = { key = key; value = value; children = child :: children}
 end
 
 module Dom = struct
   open Node
 
-  type t = Node.t
-
-  let root () = { key = "root"; value = "root"; children = M.empty }
-
-
+  let root () =  create "root" (String "rootval") []
 end
 
 include Dom
